5defd3cf8b22a9d25ca2dcf1ea3ece05905d9baf
Jam516 <kufuorkofi@gmail.com>
2022-10-13 14:27:52 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_caching import Cache
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+
+config = {
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
+connect_url = os.environ.get("POSTGRESQL")
+engine = create_engine(connect_url)
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(config)
+    cache = Cache(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PATCH,OPTIONS')
+        return response
+
+    @app.route('/')
+    @cache.cached()
+    def hacks():
+        df = pd.read_sql_table(
+            'sheet1',
+            con=engine
+        )
+        output = df.to_json(orient='records')
+        return output
+
+    return app
